94fb9be20391db0702d7b420baa71a4dea1a364e
anilbey <tuncel.manil@gmail.com>
2017-05-04 15:55:08 +0200

    jaccard scores per metadata
    Jaccard scores are computer for each metadata where:
    
    xi and xj are a true positive pair if they belong to the same
    partition in T, and they are also in the same cluster in C
    
    xi and xj are a false negative pair if they belong to the same
    partition in T, but they do not belong to the same cluster in C.
    
    xi and xj are a false positive pair if the do not belong to the
    same partition in T, but belong to the same cluster in C
    
    xi and xj are a true negative pair if they do not belong to the
    same partition in T, nor to the same cluster in C

--- Examples/ExampleHierarchicalClustering.py
+++ Examples/ExampleHierarchicalClustering.py
@@ -7,8 +7,11 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
 from OutputGenerator import OutputGenerator
+from scipy.cluster.hierarchy import fcluster
 PATH = "./GMQLData/job_data2_anil_20170427_151726_MAPPED_EXT/files"
-
+from scipy.cluster.hierarchy import cophenet
+from scipy.spatial.distance import pdist
+import operator
 
 
 #
@@ -24,19 +27,27 @@ PATH = "./GMQLData/job_data2_anil_20170427_151726_MAPPED_EXT/files"
 # print o.data.shape
 # o.data.to_pickle("2d-dataframe_count_GENES_PATIENTS")
 
+o = OutputGenerator(PATH)
+schema_file = o._get_file("schema")
+o.read_all_meta_data()
+meta = o.meta_data
 df = pd.read_pickle("2d-dataframe_count_GENES_PATIENTS")
 X = df.values.transpose()
 print X.shape
 
 # perform hierarchical clustering
 
-methods = ['single','complete','average','weighted','centroid','median','ward']
+methods = ['ward','single','complete','average','weighted','centroid','median']
+methods = ['ward']
 i = 0
 Z = []
+cluster_index = []
 for m in methods:
     Z = linkage(X, m, 'euclidean')  # cosine divides by length, use euclidean
-
-    # calculate full dendrogram
+    max_d = 17
+    cluster_index = fcluster(Z, max_d, criterion='distance')
+    print cluster_index
+    #calculate full dendrogram
     plt.figure(i)
     plt.title(m)
     plt.xlabel('sample index')
@@ -46,6 +57,45 @@ for m in methods:
         leaf_rotation=90.  # rotates the x axis labels
     )
     i = i + 1
-    
-plt.show()
 
+
+
+def calculate_jaccard(meta, metadata_key, cluster_index, sample_size):
+    TP = FP = TN = FN = 0.0
+    # pair-wise comparison
+    all_samples = range(0, sample_size)
+    unvisited_samples = range(0, sample_size)
+    for x_i in all_samples:  # for each sample
+        unvisited_samples.remove(x_i)
+        try:
+            for x_j in unvisited_samples:  # for each other sample
+                if cluster_index[x_i] == cluster_index[x_j] and meta[x_i][metadata_key] == meta[x_j][metadata_key]:
+                    TP = TP + 1
+                elif cluster_index[x_i] == cluster_index[x_j] and meta[x_i][metadata_key] != meta[x_j][metadata_key]:
+                    FP = FP + 1
+                elif cluster_index[x_i] != cluster_index[x_j] and meta[x_i][metadata_key] == meta[x_j][metadata_key]:
+                    FN = FN + 1
+                else:
+                    TN = TN + 1
+        except:
+            continue
+    try:
+        jaccard_score = TP / (TP + FP + FN)
+    except:
+        jaccard_score = 0
+    return jaccard_score
+
+jaccard_scores = {}
+for key, value in meta[0].iteritems():
+    print "jaccard score for " + key + ":"
+    res = calculate_jaccard(meta, key, cluster_index, X.shape[0])
+    jaccard_scores[key] = res
+
+sorted_dict_view = sorted(jaccard_scores.items(), key=operator.itemgetter(1), reverse=True)
+
+print sorted_dict_view
+jaccard_file = open('jaccard_scores.txt', 'a')
+for item in sorted_dict_view:
+    print>> jaccard_file, item
+
+plt.show()
